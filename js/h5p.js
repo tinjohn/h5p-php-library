e78b29927e6e313b5b7bf2d49d8d802cc273a6cd
tinjohn <tina.john@th-luebeck.de>
2025-11-12 19:05:10 +0100

    fix error 153 interactive video

--- js/h5p.js
+++ js/h5p.js
@@ -390,7 +390,8 @@ H5P.init = function (target) {
       $iframe.attr('src', 'about:blank');
     }
     else {
-      writeDocument();
+       $iframe.on('load', writeDocument);
+      // writeDocument();
     }
   });
 };
